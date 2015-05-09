8a180af6a239868b032c01000bf3e80e3a13c778
Jacqueline Hudson <jacqui.hudson@gmail.com>
2015-05-09 02:18:31 +0000

    add or update

--- ordinals.rb
+++ ordinals.rb
@@ -5,8 +5,14 @@ num_reverse = number.to_s.reverse.to_i
 digits = num_reverse.to_s.split('')
 
 if digits[0] <= "3" && digits[1] != "1"
-  # code for 1st 2nd 3rd
-  puts "That's the #{number}st nd or rd item!"
+  case digits[0]
+    when  "1"
+      puts "That's the #{number}st item!"
+    when "2"
+      puts "That's the #{number}nd item!"
+    when "3"
+      puts "That's the #{number}rd item!"
+   end
 else  
   puts "That's the #{number}th item!"
 end
